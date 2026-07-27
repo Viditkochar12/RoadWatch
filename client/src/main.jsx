a82bc06e1c82aa278bb3426496ce66f134191d1d
Vidit Kochar <kocharvidit7@gmail.com>
2026-07-27 15:42:28 +0530

    Fix Leaflet marker icons and routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,20 @@ import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
